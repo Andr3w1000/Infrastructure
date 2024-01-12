61fa89de15a22c01f28a476ad15d577079b4d000
Andr3w1000 <andr3w1000@gmail.com>
2024-01-12 14:00:38 +0100

    Update

--- dev/main.tf
+++ dev/main.tf
@@ -2,19 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-module "common" {
-  source = "../shared"
-}
+# module "common" {
+#   source = "../shared"
+# }
 
-locals {
-  merged_tags = merge(
-    module.common.global_default_tags,
-    var.default_tags
-  )
-}
+# locals {
+#   merged_tags = merge(
+#     module.common.global_default_tags,
+#     var.default_tags
+#   )
+# }
 
 
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = "ETL-project"
@@ -29,47 +29,47 @@ resource "azurerm_storage_account" "storageAcc" {
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
   is_hns_enabled           = "true"
-  tags                     = local.merged_tags
+  tags                     = var.default_tags
 }
 
-# HACK: Role assignment is needed to apply adls gen2 filesystem changes
-resource "azurerm_role_assignment" "data_role" {
-  scope                = azurerm_storage_account.storageAcc.id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-resource "azurerm_role_assignment" "contributor_role" {
-  scope                = azurerm_storage_account.storageAcc.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
+# # HACK: Role assignment is needed to apply adls gen2 filesystem changes
+# resource "azurerm_role_assignment" "data_role" {
+#   scope                = azurerm_storage_account.storageAcc.id
+#   role_definition_name = "Storage Blob Data Owner"
+#   principal_id         = data.azurerm_client_config.current.object_id
+# }
+# resource "azurerm_role_assignment" "contributor_role" {
+#   scope                = azurerm_storage_account.storageAcc.id
+#   role_definition_name = "Contributor"
+#   principal_id         = data.azurerm_client_config.current.object_id
+# }
 
-resource "azurerm_storage_data_lake_gen2_filesystem" "bronze" {
-  name               = "bronze"
-  storage_account_id = azurerm_storage_account.storageAcc.id
+# resource "azurerm_storage_data_lake_gen2_filesystem" "bronze" {
+#   name               = "bronze"
+#   storage_account_id = azurerm_storage_account.storageAcc.id
 
-  properties = {
-    hello = "aGVsbG8="
-  }
-  depends_on = [azurerm_role_assignment.contributor_role]
-}
+#   properties = {
+#     hello = "aGVsbG8="
+#   }
+#   depends_on = [azurerm_role_assignment.contributor_role]
+# }
 
-resource "azurerm_storage_data_lake_gen2_filesystem" "silver" {
-  name               = "silver"
-  storage_account_id = azurerm_storage_account.storageAcc.id
+# resource "azurerm_storage_data_lake_gen2_filesystem" "silver" {
+#   name               = "silver"
+#   storage_account_id = azurerm_storage_account.storageAcc.id
 
-  properties = {
-    hello = "aGVsbG8="
-  }
-  depends_on = [azurerm_role_assignment.contributor_role]
-}
+#   properties = {
+#     hello = "aGVsbG8="
+#   }
+#   depends_on = [azurerm_role_assignment.contributor_role]
+# }
 
-resource "azurerm_storage_data_lake_gen2_filesystem" "gold" {
-  name               = "gold"
-  storage_account_id = azurerm_storage_account.storageAcc.id
+# resource "azurerm_storage_data_lake_gen2_filesystem" "gold" {
+#   name               = "gold"
+#   storage_account_id = azurerm_storage_account.storageAcc.id
 
-  properties = {
-    hello = "aGVsbG8="
-  }
-  depends_on = [azurerm_role_assignment.contributor_role]
-}
+#   properties = {
+#     hello = "aGVsbG8="
+#   }
+#   depends_on = [azurerm_role_assignment.contributor_role]
+# }
