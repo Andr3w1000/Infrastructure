eda6cc3c0c7e295b0ff67359747e132b81604706
Andr3w1000 <andr3w1000@gmail.com>
2025-02-17 17:39:17 +0100

    dsfad

--- main.tf
+++ main.tf
@@ -1,22 +1,14 @@
-resource "azurerm_resource_group" "resourcegroups" {
+resource "azurerm_resource_group" "rg" {
     name        = var.ResourceGroup
     location    = var.Location
 }
 
-# data "azurerm_service_principal" "devOps" {
-#   display_name = "GitHubDevOps"  # Replace with your Service Principal's Display Name
-# }
-
-resource "azurerm_role_assignment" "roleassignments" {
-    principal_id   = "823aa1dc-d6f9-421a-bf26-70240d155855"
-    role_definition_name = "Contributor"
-    scope = azurerm_resource_group.resourcegroups.id
-}
-
-resource "azurerm_storage_account" "storageaccounts" {
-    name                     = "dtlstorageaccountbronze"
-    resource_group_name      = azurerm_resource_group.resourcegroups.name
-    location                 = azurerm_resource_group.resourcegroups.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_storage_account" "storageAcc" {
+  name                     = "bandidatalake"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
 }
