b518b0eef948f65e215339684ee50c4848b9bf24
Andr3w1000 <andr3w1000@gmail.com>
2025-02-18 08:31:38 +0100

    dsfa

--- providers.tf
+++ providers.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "4.16.0"
+      source  = "hashicorp/azurerm"
+      version = "4.16.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
   }
 }
 provider "azurerm" {
