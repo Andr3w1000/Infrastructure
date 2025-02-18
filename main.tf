16a0040fbd13c898aeff9a56118c95ecb29edd14
Andr3w1000 <andr3w1000@gmail.com>
2025-02-18 08:44:20 +0100

    fix

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storageAcc" {
-  name                     = "bandidatalake-${var.Environment}"
+  name                     = "bandidatalake${var.Environment}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
