9800d102f551978a0c0aff7feb461bd2c04cb5f8
Andr3w1000 <andr3w1000@gmail.com>
2025-03-30 18:36:40 +0200

    remove backend

--- qa/backend.tf
+++ qa/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-	resource_group_name     = "terraform-state-rg"
-	storage_account_name    = "tfeustorageaccount"
-	container_name          = "tfstate"
-	key                     = "qa.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+# 	resource_group_name     = "terraform-state-rg"
+# 	storage_account_name    = "tfeustorageaccount"
+# 	container_name          = "tfstate"
+# 	key                     = "qa.terraform.tfstate"
+#   }
+# }
